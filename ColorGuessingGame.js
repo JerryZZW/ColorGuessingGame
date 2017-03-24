40e2341c5616d31d634b8c5a3a38caa65fc248ed
Zhewei Zhang <qq574752690@gmail.com>
2017-03-23 19:37:19 -0700

    stylling and all the functions completed

--- ColorGuessingGame.js
+++ ColorGuessingGame.js
@@ -1,4 +1,5 @@
-var colors = generateRandomColor(6);
+var numSquares = 6;
+var colors = generateRandomColor(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -6,9 +7,52 @@ colorDisplay.textContent = pickedColor;
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
+//Easy button
+easyBtn.addEventListener("click", function() {
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares = 3;
+
+	colors = generateRandomColor(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+
+	for (var i = 0; i < squares.length; i++) {
+		if(colors[i]) {
+			squares[i].style.background = colors[i];
+		}
+		else {
+			squares[i].style.display = "none";
+		}
+	}
+
+	h1.style.background = "steelblue";
+});
+
+//Hard button
+hardBtn.addEventListener("click", function() {
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+	numSquares = 6;
+
+	colors = generateRandomColor(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors[i];
+		squares[i].style.display = "block";
+	}
+
+	h1.style.background = "steelblue";
+});
+
+//Reset button
 resetButton.addEventListener("click", function() {
-	colors = generateRandomColor(6);
+	colors = generateRandomColor(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
 
@@ -16,7 +60,9 @@ resetButton.addEventListener("click", function() {
 		squares[i].style.background = colors[i];
 	}
 
-	h1.style.background = "#232323";
+	h1.style.background = "steelblue";
+	messageDisplay.textContent = "";
+	this.textContent = "New Colors";
 });
 
 for (var i = 0; i < squares.length; i++) {
